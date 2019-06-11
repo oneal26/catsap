aeeaba020b9b6de9bb6b6bb88cd804f85e8737a6
Reed <roneal20@gmail.com>
2019-06-11 11:59:02 -0400

    testing new path for nav bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -51,10 +51,10 @@
 <nav>
 	<ul>
 		<li><a class='{segment === undefined ? "selected" : ""}' href='.'>home</a></li>
-		<li><a class='{segment === "about" ? "selected" : ""}' href='about'>about</a></li>
+		<li><a class='{segment === "about" ? "selected" : ""}' href='about/index.html'>about</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
+		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog/index.html'>blog</a></li>
 	</ul>
 </nav>
