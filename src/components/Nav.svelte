b5b9903f8484b12cdb11a351fbc146e27c1ea5b7
Reed <roneal20@gmail.com>
2019-06-11 22:31:59 -0400

    configuring and I'm too lazy to write a message everytime so I made a script

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,7 +4,7 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
+		border-bottom: 1px solid rgba(46, 68, 27,0.1);
 		font-weight: 300;
 		padding: 0 1em;
 	}
@@ -36,7 +36,7 @@
 		content: '';
 		width: calc(100% - 1em);
 		height: 2px;
-		background-color: rgb(255,62,0);
+		background-color: #2e441b;
 		display: block;
 		bottom: -1px;
 	}
@@ -55,6 +55,6 @@
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>blog</a></li>
+		<li><a rel=prefetch class='{segment === "blog" ? "selected" : ""}' href='blog'>jams</a></li>
 	</ul>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
 <style>
-	h1, figure, p {
+
+	@font-face {
+    font-family: 'coop';
+    src: url('coop.ttf');
+    font-weight: normal;
+    font-style: normal;
+	}
+
+	h1, h3, figure, p {
 		text-align: center;
 		margin: 0 auto;
 	}
@@ -9,6 +17,13 @@
 		text-transform: uppercase;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
+		color:#2e441b;
+		font-family: 'coop' arial, sans-serif;
+	}
+
+	hr {
+		color:black;
+		border: .01em black solid;
 	}
 
 	figure {
@@ -29,17 +44,23 @@
 		h1 {
 			font-size: 4em;
 		}
+		h3 {
+			font-size: 2em;
+		}
 	}
 </style>
 
 <svelte:head>
-	<title>Reed's blog</title>
+	<title>Reed O'Neal</title>
 </svelte:head>
 
-<h1>Welcome to My Blog!</h1>
-
+<h1>B O T A N I C A L</h1>
+<h3>9 / 17 / 2019</h3>
+<br>
+<hr>
+<br>
 <figure>
-	<img alt='Reed As A Cat' src='reedcat.png'>
+	<img alt='Reed As A Cat' src='botanicalscreenplaycover.jpg'>
 	<figcaption><em>welcome!</em></figcaption>
 </figure>
 
