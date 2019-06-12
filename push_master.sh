3b5c2bbe8f7a9e4f1f2d1a59a3df4c55fcbaa9cc
Reed <roneal20@gmail.com>
2019-06-11 22:50:10 -0400

    merging feature please hold...

--- push_master.sh
+++ push_master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git pull origin feature
 git checkout master
-git add .
-git commit -m "merging master please hold..."
-git push origin master
+git merge feature
+# git add .
+# git commit -m "merging master please hold..."
+# git push origin master
