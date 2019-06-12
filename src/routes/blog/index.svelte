edde284669e40917f04ae1b88f1bf660323f054f
Reed <roneal20@gmail.com>
2019-06-11 22:55:19 -0400

    merging feature please hold...

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -18,17 +18,17 @@
 </style>
 
 <svelte:head>
-	<title>Blog</title>
+	<title>jams</title>
 </svelte:head>
 
-<h1>Recent posts</h1>
-
+<h1>Under construction.</h1>
+<!-- 
 <ul>
 	{#each posts as post}
 		<!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
+		<!-- <li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
 	{/each}
-</ul>
+</ul> --> 
