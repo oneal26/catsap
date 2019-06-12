af53fb5a7244c955248408597eb86ddea1aae451
Reed <roneal20@gmail.com>
2019-06-11 22:51:08 -0400

    merging master please hold...

--- push_master.sh
+++ push_master.sh
@@ -2,6 +2,6 @@
 
 git checkout master
 git merge feature
-# git add .
-# git commit -m "merging master please hold..."
-# git push origin master
+git add .
+git commit -m "merging master please hold..."
+git push origin master
