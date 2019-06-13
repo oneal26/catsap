408f5ad906e792b0862ea1f3cd90bd017be7da3e
Reed <roneal20@gmail.com>
2019-06-12 21:42:12 -0400

    merging feature please hold...

--- src/routes/screenplay.svelte
+++ src/routes/screenplay.svelte
@@ -63,7 +63,7 @@
 
 <p>Under construction.</p>
 <p>
-	<a href="pdf/BotanicalUnlocked.pdf">e n j o i</a>
+	<a href="screenplay">e n j o i</a>
 	<br>
-	<object height="1000em" width="100%" data="pdf/BotanicalUnlocked.pdf" type="application/pdf"></object>
+	<object height="1000em" width="100%" data="BotanicalLocked.pdf" type="application/pdf"></object>
 </p>
