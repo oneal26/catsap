6319003efae0814e29949c7921a1972fde4f7c5e
Reed <roneal20@gmail.com>
2019-09-12 22:45:59 -0400

    merging feature please hold...

--- src/routes/jams/index.svelte
+++ src/routes/jams/index.svelte
@@ -1,14 +1,3 @@
-<script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
-	}
-</script>
-
-<script>
-	export let posts;
-</script>
 
 <style>
 	ul {
