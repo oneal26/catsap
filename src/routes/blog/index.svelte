bf1f36b2eba9ac200fba01a261cf00678a5435b9
Reed <roneal20@gmail.com>
2019-09-12 22:30:17 -0400

    merging feature please hold...

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -71,7 +71,33 @@
 	<title>jams</title>
 </svelte:head>
 
-<h1>Under construction.</h1>
+<h1>1. Sunrise</h1>
+<h1>2. Been A While</h1>
+<h1>3. The Perfect Nap</h1>
+<h1>4. Fade</h1>
+<h1>5. Introspective</h1>
+<h1>6. Skyline</h1>
+<h1>7. Smoke</h1>
+<h1>8. Coffee @ Night </h1>
+<h1>9. End Of The World</h1>
+<h1>10. Feel Me</h1>
+<h1>11. Nightmare</h1>
+<h1>12. Mixed Feelings</h1>
+<h1>13. Familiar</h1>
+<h1>14. Kontaqt</h1>
+<h1>15. An Optimistic Letter</h1>
+<h1>16. Sunset</h1>
+<h1>17. Pick It Up</h1>
+<h1>18. Sea Glass [DRAFT]</h1>
+<h1>19. Wonderful</h1>
+<h1>20. Mirror</h1>
+<h1>21. End Of Summer</h1>
+<h1>22. Smile</h1>
+<h1>23. Timeless</h1>
+<h1>24. I.V.</h1>
+<h1>25. Space Static</h1>
+<h1>26. Raspberry</h1>
+<h1>27. Change</h1>
 <!-- 
 <ul>
 	{#each posts as post}
