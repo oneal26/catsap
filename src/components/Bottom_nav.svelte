816d11c10bdc517d4bdd8a5046a15e6c3a11d62d
Reed <roneal20@gmail.com>
2020-09-11 11:38:59 -0400

    merging feature please hold...

--- src/components/Bottom_nav.svelte
+++ src/components/Bottom_nav.svelte
@@ -68,12 +68,12 @@
 
 <nav class="fixed-bottom bg">
 	<ul>
-		<li><iframe src="https://open.spotify.com/follow/1/?uri=spotify:artist:4tCGRoI7zhPtJvKoyZEJES?si=E5p9yV0PQuqv7GZeOmfD0w&size=basic&theme=light" width="200" height="25" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowtransparency="true"></iframe></li>
+		<!-- <li><iframe src="https://open.spotify.com/follow/1/?uri=spotify:artist:4tCGRoI7zhPtJvKoyZEJES?si=E5p9yV0PQuqv7GZeOmfD0w&size=basic&theme=light" width="200" height="25" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowtransparency="true"></iframe></li> -->
 		<!-- Right Nav Items -->
 		<li class="right"><a href="https://soundcloud.com/reedinbooks/sets/botanical-side-a"><img src="soundcloud.png" alt="SoundCloud" class="icon"></a></li>
-		<!-- <li class="right"><a href="#"><img src="tidal.jpg" alt="Tidal" class="icon"></a></li> -->
-		<li class="right"><a href="https://open.spotify.com/album/07Dpa9PyululGEdXbPTPij?si=nI0DXluqSEyx-Hih9zLGEQ"><img src="spotify.png" alt="Spotify" class="icon"></a></li>
-		<li class="right"><a href="https://music.apple.com/us/album/botanical-side-a/1480486916"><img src="apple.png" alt="Apple Music" class="icon"></a></li>
+		<li class="right"><a href="https://listen.tidal.com/artist/21190538"><img src="tidal.jpg" alt="Tidal" class="icon"></a></li>
+		<li class="right"><a href="https://open.spotify.com/artist/6lSk8iQDQIHdX0eRkSNqfQ?si=hI31wp1ERo6c3YMWznCO_A"><img src="spotify.png" alt="Spotify" class="icon"></a></li>
+		<li class="right"><a href="https://music.apple.com/gb/artist/reed/1529653002"><img src="apple.png" alt="Apple Music" class="icon"></a></li>
 
 	</ul>
 
